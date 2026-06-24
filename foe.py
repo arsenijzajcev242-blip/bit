a5899ed10bdddc72d8db6c0628c9caf17efafbeb
arsenijzajcev242-blip <arsenijzajcev242@gmail.com>
2026-06-24 17:22:01 +0300

    Update foe.py

--- foe.py
+++ foe.py
@@ -21,7 +21,8 @@ async def get_photo(message: types.Message):
     us_id = message.from_user.id
     if us_id in users:
         await message.answer_photo(photo=users[us_id])
-
+    else:
+        await message.answer('Вы еще не отправляли фото')
 
 async def main():
     await dp.start_polling(bot)
