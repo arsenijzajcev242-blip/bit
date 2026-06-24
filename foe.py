62cb9c46cd414bc12d6157ffa4945be76db1d927
arsenijzajcev242-blip <arsenijzajcev242@gmail.com>
2026-06-24 17:16:51 +0300

    Update foe.py

--- foe.py
+++ foe.py
@@ -1,9 +1,9 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import CommandStart, Command
 
 TOKEN = '8753338258:AAHkpPFzt6uJMJ2DKUF-_jAvMJI_746WIZ4'
-
+users = {}
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
@@ -11,11 +11,26 @@ dp = Dispatcher()
 async def echo(message: types.Message):
     await message.answer('Привет')
 
+@dp.message(F.photo)
+async def pgoto(message: types.Message):
+    photo = message.photo[-1].file_id
+    users[message.from_user.id] = photo
+    await message.answer("Фото сохранено.")
+@dp.message(Command('get_photo'))
+async def get_photo(message: types.Message):
+    us_id = message.from_user.id
+    if us_id in users:
+        await message.answer_photo(photo=users[us_id])
+
+
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
 
 
 
