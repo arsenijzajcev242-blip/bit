d2dd61078c466e9f4e4057dc9d39fc50e2937dec
arsenijzajcev242-blip <arsenijzajcev242@gmail.com>
2026-06-24 17:39:13 +0300

    Update foe.py

--- foe.py
+++ foe.py
@@ -1,31 +1,14 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters import CommandStart, Command
+from aiogram import Bot, Dispatcher
 from config import TOKEN
+from app.handlers import router
 
-users = {}
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message(CommandStart())
-async def echo(message: types.Message):
-    await message.answer('Привет')
-
-@dp.message(F.photo)
-async def pgoto(message: types.Message):
-    photo = message.photo[-1].file_id
-    users[message.from_user.id] = photo
-    await message.answer("Фото сохранено.")
-@dp.message(Command('get_photo'))
-async def get_photo(message: types.Message):
-    us_id = message.from_user.id
-    if us_id in users:
-        await message.answer_photo(photo=users[us_id])
-    else:
-        await message.answer('Вы еще не отправляли фото')
-
 async def main():
-    await dp.start_polling(bot)
+    dp.include_router(router)
+    await router.start_polling(bot)
 
 if __name__ == "__main__":
     try:
