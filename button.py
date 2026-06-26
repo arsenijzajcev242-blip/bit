9cf21d0cfe9113e438ce3afe0e9c78490ce4d041
arsenijzajcev242-blip <arsenijzajcev242@gmail.com>
2026-06-26 22:35:03 +0300

    Update button.py

--- button.py
+++ button.py
@@ -1,11 +1,27 @@
-from aiogram.types import (InlineKeyboardButton, KeyboardButton, InlineKeyboardMarkup)
-from aiogram.utils.keyboard import InlineKeyboardBuilder , ReplyKeyboardBuilder , ReplyKeyboardMarkup
+from aiogram.types import (
+    InlineKeyboardButton,
+    KeyboardButton
+)
+from aiogram.utils.keyboard import (
+    InlineKeyboardBuilder,
+    ReplyKeyboardBuilder
+)
 
+# Обычная клавиатура
 main = ReplyKeyboardBuilder()
-btn1 = KeyboardButton(text='Профиль')
-btn2 = KeyboardButton(text='Корзина')
-btn3 = KeyboardButton(text='Меню')
-main.row(btn1,btn2,btn3)
+
+main.row(
+    KeyboardButton(text='Профиль'),
+    KeyboardButton(text='Корзина'),
+    KeyboardButton(text='Меню')
+)
+
+# Inline-клавиатура
 menu = InlineKeyboardBuilder()
-btn1menu = InlineKeyboardButton(text='Каталог',callback_data = 'btn1')
-menu.add(btn1menu)
+
+menu.add(
+    InlineKeyboardButton(
+        text='Каталог',
+        callback_data='catalog'
+    )
+)
